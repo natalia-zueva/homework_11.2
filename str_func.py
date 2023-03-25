af6adc56778fc5312dc0aedac167a31f3bf1ce1d
Natalia Zueva <zueva_natalia@mail.ru>
2023-03-25 22:41:49 +0500

    added new function and docstrings in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def convert_str(string):
+    """Возвращает все буквы в словах заглавными"""
     upper_letters_str = string.upper()
     return upper_letters_str
+def convert_first_letter(string):
+    """Возвращает слова с заглавными первыми буквами"""
+    first_letter_str = string.title()
+    return first_letter_str
